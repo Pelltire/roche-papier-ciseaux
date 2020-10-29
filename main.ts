c83b5cc3a8abff9fc0f2ca4a5786eb202e554366
Pelltire <73190861+Pelltire@users.noreply.github.com>
2020-10-29 14:19:53 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+let tool = 0
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else {
+        if (tool == 1) {
+            basic.showIcon(IconNames.Square)
+        } else {
+            if (tool == 2) {
+                basic.showIcon(IconNames.Scissors)
+            }
+        }
+    }
+})
+basic.forever(function () {
+	
+})
